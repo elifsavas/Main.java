93b3942b1917b62dbb86bb69d4ba2f4348fcdbce
Elif Savaş <98321352+elifsavas@users.noreply.github.com>
2022-10-31 23:27:42 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int k, m;
+        int total =1 ;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Üssü Alınacak Sayıyı Giriniz: ");
+        k = input.nextInt();
+
+        System.out.print("Üs Olacak Sayıyı Giriniz: ");
+        m = input.nextInt();
+
+        for (int i=1 ; i <= m ; i++ ) {
+
+            total *= k ;
+
+        }
+
+        System.out.println(total);
     }
 }
